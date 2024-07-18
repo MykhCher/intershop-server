b79d0e0acf76a73e6d676052a510eeedba0665ad
MykhCher <micher1240@gmail.com>
2024-07-18 13:18:56 +0300

    add dotenv

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const app = require('./src/app');
 
 const http = require('http');
 
+require('dotenv').config();
+
+const port = process.env.SERVER_PORT;
+const hostName = process.env.HOST_NAME;
+const isSecure = Number(process.env.SERVER_IS_SECURE);
+
 const server = http.createServer(app);
-server.listen('5000', '127.0.0.1', () => {console.log('Listening')})
+server.listen(port, hostName, () => {
+    const address = `http${isSecure ? 's' : ''}://${hostName}:${port}`
+    console.log(`Server listening to ${address}`);
+});
