30df028a947c752b992a37ba0963183ec8cc91ed
MykhCher <micher1240@gmail.com>
2024-07-18 13:35:07 +0300

    add connection check

--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 const app = require('./src/app');
 
 const http = require('http');
+const db = require('./src/db/models');
+
 
 require('dotenv').config();
+
 
 const port = process.env.SERVER_PORT;
 const hostName = process.env.HOST_NAME;
 const isSecure = Number(process.env.SERVER_IS_SECURE);
 
+
 const server = http.createServer(app);
+
 server.listen(port, hostName, () => {
     const address = `http${isSecure ? 's' : ''}://${hostName}:${port}`
     console.log(`Server listening to ${address}`);
+    db.sequelize.authenticate()
+        .then(() => {console.log('Database connection was successfully established!')})
+        .catch(err => {console.log(`Database connection error: ${err.message}`)});
 });
