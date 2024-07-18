60b9590bc58326cdcf01f49f2bfde4bc1fa94d7a
MykhCher <micher1240@gmail.com>
2024-07-18 12:52:39 +0300

    initialize express server

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const app = require('./src/app');
+
+const http = require('http');
+
+const server = http.createServer(app);
+server.listen('5000', '127.0.0.1', () => {console.log('Listening')})
