e2aeacfa40c5a0adad5079f1515ed67428a64b3a
MykhCher <micher1240@gmail.com>
2024-08-08 17:25:31 +0300

    get item types in stores

--- src/controllers/miscController.js
+++ src/controllers/miscController.js
@@ -10,6 +10,26 @@ class MiscController {
             .catch(next);
     }
 
+    itemTypesInStores(req, res, next) {
+        db.Item.findAll({
+            attributes: [[db.Sequelize.fn('SUM', db.Sequelize.col('amount')), 'total_amount']],
+            include: [db.Store, db.Type],
+            group: ['Store.id', 'Type.id'],
+            raw: true
+        })
+            .then(rows => {
+                const result = rows.map(row => {
+                    return {
+                        "type_title": row['Type.title'],
+                        "store_title": row['Store.title'],
+                        "total_amount": Number(row.total_amount),
+                    }
+                });
+
+                res.json(result);
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new MiscController();
--- src/routers/miscRouter.js
+++ src/routers/miscRouter.js
@@ -6,5 +6,6 @@ const { Router } = require('express');
 const miscRouter = Router();
 
 miscRouter.get('/modelsByBrands', miscController.modelsByBrands);
+miscRouter.get('/reviewStores', miscController.itemTypesInStores);
 
 module.exports = miscRouter;
